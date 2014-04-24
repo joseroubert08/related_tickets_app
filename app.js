a4fe19521107d92107dfa6845bc1e9f44561920c
Pi3r <pierre.nespo@gmail.com>
2014-04-24 11:14:22 +0100

    refactor app to stop using the requireProperties logic since we only listen for the subject as a property

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 (function() {
 
   return {
-
-    currAttempt: 0,
-
-    MAX_ATTEMPTS: 20,
-
-    relatedTickets: {},
-
-    defaultState: 'loading',
-
     requests: {
       search: function(params) {
         return {
@@ -20,20 +11,67 @@
     },
 
     events: {
-      'app.created'               : 'onAppCreated',
-      'keydown #search-input'     : 'handleKeydown',
-      'click .search'             : 'handleClick',
-      'requiredProperties.ready'  : 'handleRequiredProperties',
-      'search.done'               : 'handleResults',
-      'ticket.subject.changed'    : 'handleSubjecChanged'
+      'app.created'             : 'onTicketSubjectChanged',
+      'ticket.subject.changed'  : 'onTicketSubjectChanged',
+      'keydown #search-input'   : 'onSearchKeyPressed',
+      'click .search'           : 'onSearchClicked',
+      'search.done'             : 'onSearchDone',
+      'search.fail'             : 'onSearchFailed'
     },
 
-    onAppCreated: function() {
-      this.requiredProperties = [
-        'ticket.subject'
-      ];
+    onTicketSubjectChanged: _.debounce(function() {
+      if (_.isEmpty(this.ticket().subject())) { return; }
 
-      this.allRequiredPropertiesExist();
+      var keywords = this.extractKeywords(this.ticket().subject()).join(" ");
+
+      this.$('.search-input').val(keywords);
+
+      this.searchTickets(keywords);
+    }, 400),
+
+    onSearchKeyPressed: function(e) {
+      var query = this.$(e.target).val();
+
+      if (e.which === 13 && !_.isEmpty(query)) {
+        e.preventDefault();
+
+        this.searchTickets(query);
+      }
+    },
+
+    onSearchClicked: function(e) {
+      e.preventDefault();
+
+      var query = this.$('.search-input').val();
+
+      if (!_.isEmpty(query)) {
+        this.searchTickets(query);
+      }
+    },
+
+    onSearchDone: function(data) {
+      var currentTicketId = this.ticket().id(),
+          tickets = data.results.slice(0,10);
+
+      if (currentTicketId) {
+        // remove current ticket from results
+        tickets = _.reject(tickets, function(ticket) {
+          return ticket.id === currentTicketId;
+        });
+      }
+
+      _.each(tickets, function(ticket) {
+        ticket.description = ticket.description.substr(0,300).concat("...");
+      });
+
+      this.switchTo('results', {
+        tickets: tickets,
+        tooltip_enabled: !this.setting('disable_tooltip')
+      });
+    },
+
+    onSearchFailed: function() {
+      this.showError();
     },
 
     searchTickets: function(keywords){
@@ -44,61 +82,6 @@
 
       this.ajax('search', params);
     },
-
-    handleKeydown: function(e) {
-      var query = this.$(e.target).val();
-
-      if (e.which === 13) {
-        if (query.length > 2) this.searchTickets(query);
-        return false;
-      }
-    },
-
-    handleClick: function(e) {
-      e.preventDefault();
-
-      var query = this.$(".search-wrapper").find("#search-input").val();
-
-      if (query.length > 2) this.searchTickets(query);
-    },
-
-    handleRequiredProperties: function() {
-      var keywords = this.extractKeywords(this.ticket().subject()).join(" ");
-
-      this.$('#search-input').val(keywords);
-
-      this.searchTickets(keywords);
-    },
-
-    handleResults: function(data) {
-      var ticketId = this.ticket().id();
-
-      if (ticketId) {
-        // remove current ticket from results
-        this.relatedTickets.results = _.reject(data.results, function(result) {
-          return result.id === ticketId;
-        });
-      } else {
-        this.relatedTickets.results = data.results;
-      }
-
-      if (this.relatedTickets.results.length > 10) {
-        this.relatedTickets.results = this.relatedTickets.results.slice(0,10);
-      }
-
-      _.each(this.relatedTickets.results, function(result) {
-        result.description = result.description.substr(0,300).concat("...");
-      });
-
-      this.switchTo('results', {
-        data: this.relatedTickets,
-        tooltip_enabled: !this.setting('disable_tooltip')
-      });
-    },
-
-    handleSubjecChanged: _.debounce(function() {
-      this.handleRequiredProperties();
-    }, 400),
 
     extractKeywords: function(text) {
       // strip punctuation and extra spaces
@@ -114,56 +97,11 @@
       return keywords;
     },
 
-    allRequiredPropertiesExist: function() {
-      if (this.requiredProperties.length > 0) {
-        var valid = this.validateRequiredProperty(this.requiredProperties[0]);
-
-        // prop is valid, remove from array
-        if (valid) {
-          this.requiredProperties.shift();
-        }
-
-        if (this.requiredProperties.length > 0 && this.currAttempt < this.MAX_ATTEMPTS) {
-          if (!valid) {
-            ++this.currAttempt;
-          }
-
-          _.delay(_.bind(this.allRequiredPropertiesExist, this), 100);
-          return;
-        }
-      }
-
-      if (this.currAttempt < this.MAX_ATTEMPTS) {
-        this.trigger('requiredProperties.ready');
-      } else {
-        this.showError(null, this.I18n.t('global.error.data'));
-      }
-    },
-
-    safeGetPath: function(propertyPath) {
-      return _.inject( propertyPath.split('.'), function(context, segment) {
-        if (context == null) { return context; }
-        var obj = context[segment];
-        if ( _.isFunction(obj) ) { obj = obj.call(context); }
-        return obj;
-      }, this);
-    },
-
-    validateRequiredProperty: function(propertyPath) {
-      var value = this.safeGetPath(propertyPath);
-      return value != null && value !== '' && value !== 'no';
-    },
-
     showError: function(title, msg) {
       this.switchTo('error', {
         title: title || this.I18n.t('global.error.title'),
         message: msg || this.I18n.t('global.error.message')
       });
-    },
-
-    handleFail: function() {
-      // Show fail message
-      this.showError();
     }
 
   };
